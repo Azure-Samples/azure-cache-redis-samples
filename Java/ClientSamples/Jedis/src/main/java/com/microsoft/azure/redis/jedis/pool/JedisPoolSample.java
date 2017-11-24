7d0553d7dca328718f3ccb59261ae45d12ee8fce
Warren Zhu <zhonzh@microsoft.com>
2017-11-24 14:48:30 +0800

    Add system level metric log such as memory, cpu load to support RedisConnectionException diagnosis

--- Java/ClientSamples/Jedis/src/main/java/com/microsoft/azure/redis/jedis/pool/JedisPoolSample.java
+++ Java/ClientSamples/Jedis/src/main/java/com/microsoft/azure/redis/jedis/pool/JedisPoolSample.java
@@ -1,13 +1,19 @@
 package com.microsoft.azure.redis.jedis.pool;
 
+import com.sun.management.OperatingSystemMXBean;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.apache.log4j.Logger;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLPeerUnverifiedException;
 import javax.net.ssl.SSLSession;
-
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-import org.apache.log4j.Logger;
-import redis.clients.jedis.*;
+import java.lang.management.ManagementFactory;
+import java.text.DecimalFormat;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public class JedisPoolSample {
     private static JedisPool jedisPool;
@@ -26,14 +32,13 @@ public class JedisPoolSample {
     }
 
     public static void main(String args[]){
-        sampleForJedisPool();
-        logPoolCurrentUsage(getJedisPool(), jedisClientConfiguration.getPoolConfig());
+        IntStream.range(0, 10).parallel().forEach((i) -> sampleForJedisPool());
 
         // when closing your application
         getJedisPool().destroy();
     }
 
-    private static void sampleForJedisPool(){
+    public static void sampleForJedisPool(){
         try(Jedis jedis = getJedisPool().getResource()){
             String key = "foo";
             String value = "bar";
@@ -43,14 +48,19 @@ public class JedisPoolSample {
             logger.info(String.format("Current value for key %s is %s", key, newValue));
             jedis.zadd("sose", 0, "car"); jedis.zadd("sose", 0, "bike");
             Set<String> sose = jedis.zrange("sose", 0, -1);
+        } catch (JedisConnectionException e){
+            logger.warn("Failed to connect to Jedis server: ", e);
+            logPoolCurrentUsage();
         }
     }
 
-    private static void logPoolCurrentUsage(JedisPool jedisPool, GenericObjectPoolConfig poolConfig)
+    public static void logPoolCurrentUsage()
     {
         int active = jedisPool.getNumActive();
         int idle = jedisPool.getNumIdle();
         int total = active + idle;
+        GenericObjectPoolConfig poolConfig = jedisClientConfiguration.getPoolConfig();
+        logger.info(getSystemMetrics());
         logger.info(String.format(
                 "JedisPool: Active=%d, Idle=%d, Waiters=%d, total=%d, maxTotal=%d, minIdle=%d, maxIdle=%d",
                 active,
@@ -62,6 +72,28 @@ public class JedisPoolSample {
                 poolConfig.getMaxIdle()
         ));
 
+    }
+
+    private static String getSystemMetrics() {
+        OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+        String freeMemorySize = toReadableSize(Runtime.getRuntime().freeMemory());
+        String maxMemorySize = toReadableSize(Runtime.getRuntime().maxMemory());
+        String totalMemorySize = toReadableSize(Runtime.getRuntime().totalMemory());
+        String systemCpuLoad = toReadablePercent(operatingSystemMXBean.getSystemCpuLoad());
+        String processCpuLoad = toReadablePercent(operatingSystemMXBean.getProcessCpuLoad());
+
+        return String.format("Free memory: %s, Max memory: %s, Total memory: %s, System CPU load: %s, Process CPU load: %s", freeMemorySize, maxMemorySize, totalMemorySize, systemCpuLoad, processCpuLoad);
+    }
+
+    private static String toReadableSize(long size) {
+        if(size <= 0) return "0";
+        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+
+    private static String toReadablePercent(double percent){
+        return String.format("%.02f %%", percent * 100);
     }
 
     private static class SimpleHostNameVerifier implements HostnameVerifier {
@@ -84,5 +116,11 @@ public class JedisPoolSample {
         }
     }
 
+    public static JedisPoolFactory getJedisPoolFactory() {
+        return jedisPoolFactory;
+    }
 
+    public static JedisPoolConfiguration getJedisClientConfiguration() {
+        return jedisClientConfiguration;
+    }
 }
