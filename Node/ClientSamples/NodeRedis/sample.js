3374e5e6da9adb1db027d6b5762fcbbad90cc7b1
Warren Zhu <zhonzh@microsoft.com>
2017-11-29 10:53:23 +0800

    Add Node.js redis client sample

---
+++ Node/ClientSamples/NodeRedis/sample.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var redis = require('redis');
+
+// change {cacheName} into your own cache name
+// var host = '{cacheName}.redis.cache.windows.net'
+// change into your password
+// var password = 'password'
+var port = 6380;
+
+var client = redis.createClient(port, host, {auth_pass: password, tls: {servername: host}});
+
+client.on('error', function (err) {
+    console.log('error event - ' + err);
+});
+
+var key = 'foo';
+var value = 'bar';
+
+client.set(key, value, redis.print);
+client.get(key, function (err, reply) {
+    if (err) throw err;
+    console.log(reply.toString());
+});
+
+client.quit(function (err, res) {
+    console.log('redis client quit.');
+});
