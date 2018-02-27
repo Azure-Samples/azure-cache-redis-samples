269d6fbdd566cddd0132d9b7ffd5831a68d77db2
Warren Zhu <zhonzh@microsoft.com>
2018-02-27 16:02:33 +0800

    Enable adaptive topology refresh trigger in Lettuce cluster

--- Java/ClientSamples/Jedis/src/main/java/cluster/LettuceClusterFactory.java
+++ Java/ClientSamples/Jedis/src/main/java/cluster/LettuceClusterFactory.java
@@ -1,6 +1,8 @@
 package cluster;
 
 import com.lambdaworks.redis.RedisURI;
+import com.lambdaworks.redis.cluster.ClusterClientOptions;
+import com.lambdaworks.redis.cluster.ClusterTopologyRefreshOptions;
 import com.lambdaworks.redis.cluster.RedisClusterClient;
 import com.lambdaworks.redis.cluster.api.StatefulRedisClusterConnection;
 import com.lambdaworks.redis.support.ConnectionPoolSupport;
@@ -9,6 +11,8 @@ import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
+
+import java.util.concurrent.TimeUnit;
 
 public class LettuceClusterFactory {
     private RedisClientConfiguration configuration;
@@ -20,7 +24,12 @@ public class LettuceClusterFactory {
     public GenericObjectPool<StatefulRedisClusterConnection<String, String>> createInstance(){
         RedisURI redisUri = RedisURI.Builder.redis(configuration.getHostName()).withPassword(configuration.getPassword()).withSsl(configuration.isUseSsl()).withPort(configuration.getPort()).build();
         RedisClusterClient clusterClient = RedisClusterClient.create(redisUri);
-
+        ClusterTopologyRefreshOptions topologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enableAllAdaptiveRefreshTriggers()
+                .build();
+        clusterClient.setOptions(ClusterClientOptions.builder()
+                .topologyRefreshOptions(topologyRefreshOptions)
+                .build());
         return ConnectionPoolSupport
                 .createGenericObjectPool(() -> clusterClient.connect(), configuration.getPoolConfig());
 
