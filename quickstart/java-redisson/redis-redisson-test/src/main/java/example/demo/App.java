5e6be51e5529a2d2a704a8ce3fc1e880476e3e30
Jianguo Ma <jiangma@microsoft.com>
2021-11-29 15:43:54 +0800

    borrow the map naming from the Redisson samples
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- quickstart/java-redisson/redis-redisson-test/src/main/java/example/demo/App.java
+++ quickstart/java-redisson/redis-redisson-test/src/main/java/example/demo/App.java
@@ -20,19 +20,19 @@ public class App {
         RedissonClient client = Redisson.create(config);
 
         // Perform cache operations using the cache client
-        RMap<String, String> cache = client.getMap("namedCache");
+        RMap<String, String> map = client.getMap("test");
 
         // Simple get and put of string data into the cache
         System.out.println("\nCache Command  : GET Message");
-        System.out.println("Cache Response : " + cache.get("Message"));
+        System.out.println("Cache Response : " + map.get("Message"));
 
         System.out.println("\nCache Command  : SET Message");
-        System.out.println("Cache Response : " + cache.put("Message",
+        System.out.println("Cache Response : " + map.put("Message",
                 String.format("Hello! The cache is working from Java! %s", LocalDateTime.now())));
 
         // Demonstrate "SET Message" executed as expected
         System.out.println("\nCache Command  : GET Message");
-        System.out.println("Cache Response : " + cache.get("Message"));
+        System.out.println("Cache Response : " + map.get("Message"));
 
         System.exit(0);
     }
