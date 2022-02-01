7ecfbc89d55ca742032b629d1031e463449a9cc1
Jim Roberts <JimRoberts-MS@users.noreply.github.com>
2022-02-01 10:42:35 -0500

    Update nodejs redistest.js for newer client version

--- quickstart/nodejs/redistest.js
+++ quickstart/nodejs/redistest.js
@@ -1,37 +1,41 @@
 var redis = require("redis");
-var bluebird = require("bluebird");
-
-// Convert Redis client API to use promises, to make it usable with async/await syntax
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
 
 async function testCache() {
 
     // Connect to the Azure Cache for Redis over the TLS port using the key.
-    var cacheConnection = redis.createClient(6380, process.env.REDISCACHEHOSTNAME,
-        { auth_pass: process.env.REDISCACHEKEY, tls: { servername: process.env.REDISCACHEHOSTNAME } });
+    var cacheHostName = process.env.REDISCACHEHOSTNAME;
+    var cachePassword = process.env.REDISCACHEKEY;
+    var cacheConnection = redis.createClient({
+        // rediss for TLS
+        url: "rediss://" + cacheHostName + ":6380",
+        password: cachePassword,
+    });
+    await cacheConnection.connect();
 
     // Perform cache operations using the cache connection object...
 
     // Simple PING command
     console.log("\nCache command: PING");
-    console.log("Cache response : " + await cacheConnection.pingAsync());
+    console.log("Cache response : " + await cacheConnection.ping());
 
     // Simple get and put of integral data types into the cache
     console.log("\nCache command: GET Message");
-    console.log("Cache response : " + await cacheConnection.getAsync("Message"));
+    console.log("Cache response : " + await cacheConnection.get("Message"));
 
     console.log("\nCache command: SET Message");
-    console.log("Cache response : " + await cacheConnection.setAsync("Message",
+    console.log("Cache response : " + await cacheConnection.set("Message",
         "Hello! The cache is working from Node.js!"));
 
     // Demonstrate "SET Message" executed as expected...
     console.log("\nCache command: GET Message");
-    console.log("Cache response : " + await cacheConnection.getAsync("Message"));
+    console.log("Cache response : " + await cacheConnection.get("Message"));
 
     // Get the client list, useful to see if connection list is growing...
     console.log("\nCache command: CLIENT LIST");
-    console.log("Cache response : " + await cacheConnection.clientAsync("LIST"));
+    console.log("Cache response : " + await cacheConnection.sendCommand(["CLIENT", "LIST"]));
+
+    console.log("\nDone");
+    process.exit();
 }
 
 testCache();
