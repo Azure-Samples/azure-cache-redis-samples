ceed05f95eedc1abc4b0f2c1702a4c44cd556b59
Stanley Small <103065678+stanleysmall-microsoft@users.noreply.github.com>
2022-06-30 12:04:48 -0400

    Update BundleConfig.cs

--- quickstart/aspnet/ContosoTeamStats/App_Start/BundleConfig.cs
+++ quickstart/aspnet/ContosoTeamStats/App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace ContosoTeamStats
         {
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{$version}.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
