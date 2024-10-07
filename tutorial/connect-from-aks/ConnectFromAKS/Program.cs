bfd4d893aa3666db31f950fab8c72b549dd2ade4
Shruti Pathak <shpathak@microsoft.com>
2024-10-07 12:49:01 -0700

    updates

--- tutorial/connect-from-aks/ConnectFromAKS/Program.cs
+++ tutorial/connect-from-aks/ConnectFromAKS/Program.cs
@@ -3,7 +3,7 @@ using StackExchange.Redis;
 using static System.Console;
 
 WriteLine("This sample shows how to connect to an Azure Redis cache using an Microsoft Entra identity or an access key. " +
-    "You can also run this sample locally with your user principal configured as a Redis User for your redis instance.");
+"You can also run this sample locally with your user principal configured as a Redis User for your redis instance.");
 try
 {
     var authenticationType = Environment.GetEnvironmentVariable("AUTHENTICATION_TYPE");
@@ -22,7 +22,7 @@ try
         case "ACCESS_KEY":
             WriteLine("Connecting to {cacheHostName} with an access key..");
             var redisAccessKey = Environment.GetEnvironmentVariable("REDIS_ACCESSKEY");
-            configurationOptions = ConfigurationOptions.Parse($"{redisHostName}:{redisPort},password={redisAccessKey},ssl=True,abortConnect=False");
+            configurationOptions = ConfigurationOptions.Parse($"{redisHostName}:{redisPort},password={redisAccessKey}");
             configurationOptions.AbortOnConnectFail = true; // Fail fast for the purposes of this sample. In production code, this should remain false to retry connections on startup
             break;
 
@@ -32,20 +32,20 @@ try
     }
 
     using ConnectionMultiplexer redis = await ConnectionMultiplexer.ConnectAsync(configurationOptions);
-        // Get the default database within the Redis cache
-        var db = redis.GetDatabase();
+    // Get the default database within the Redis cache
+    var db = redis.GetDatabase();
 
-        // Set a key-value pair in Redis
-        var key = "myKey";
-        var value = "Hello, Redis!";
-        await db.StringSetAsync(key, value);
+    // Set a key-value pair in Redis
+    var key = "myKey";
+    var value = "Hello, Redis!";
+    await db.StringSetAsync(key, value);
 
-        // Retrieve the value from Redis
-        var retrievedValue = await db.StringGetAsync(key);
-        WriteLine($"Retrieved value from Redis: {retrievedValue}");
+    // Retrieve the value from Redis
+    var retrievedValue = await db.StringGetAsync(key);
+    WriteLine($"Retrieved value from Redis: {retrievedValue}");
 
-        // Close the Redis connection
-        redis.Close();
+    // Close the Redis connection
+    redis.Close();
 }
 catch (Exception ex)
 {
