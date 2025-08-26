e3300bb29989637c0254b9fba6a873453d0cd56f
Dina Berry <diberry@microsoft.com>
2025-08-26 17:15:26 +0000

    Help from Philo

--- quickstart/nodejs/src/index.ts
+++ quickstart/nodejs/src/index.ts
@@ -14,9 +14,9 @@ console.log('Using Redis endpoint:', endpointUrl);
 
 try {
 
-    function getClient() {
+    function getCluster() {
 
-        if (!resourceEndpoint) throw new Error('AZURE_MANAGED_REDIS_HOST_NAME must be set');
+        if (!endpointUrl) throw new Error('AZURE_MANAGED_REDIS_HOST_NAME must be set');
 
         const credential = new DefaultAzureCredential();
 
@@ -29,22 +29,27 @@ try {
             }
         });
 
-        const cluster = createCluster({
-        rootNodes: [{ url: endpointUrl }],
-        defaults: {
-            credentialsProvider: provider,
-            socket: { 
-                reconnectStrategy:() => new Error('Failure to connect'),
-                connectTimeout: 15000 
+        const client = createCluster({
+            rootNodes: [{ url: endpointUrl }],
+            defaults: {
+                credentialsProvider: provider,
+                socket: { 
+                    connectTimeout: 15000, 
+                    reconnectStrategy:() => new Error('Failure to connect'), 
+                    // The following 2 properties are required for Azure Managed Redis
+                    tls: true,
+                    rejectUnauthorized: false  
+                    
+                }
             }
-        }
         });
 
-        
-        cluster.on('error', (err) => console.error('Redis cluster error:', err));
-        return cluster;
+        client.on('error', (err) => console.error('Redis cluster error:', err));
+
+        return client;
     }
-    client = getClient();
+
+    client = getCluster();
 
     await client.connect();
 
@@ -68,3 +73,19 @@ try {
         }
     }
 }
+/*
+Client error: 
+Using Redis endpoint: rediss://diberry-managed-redis.eastus2.redis.azure.net:10000
+Ping result: PONG
+Error: SimpleError: MOVED 999 4.150.65.49:8501
+    at #decodeSimpleError (/workspaces/azure-cache-redis-samples/quickstart/nodejs/node_modules/@redis/client/dist/lib/RESP/decoder.js:459:13)
+    at #decodeTypeValue (/workspaces/azure-cache-redis-samples/quickstart/nodejs/node_modules/@redis/client/dist/lib/RESP/decoder.js:104:91)
+    at Decoder.write (/workspaces/azure-cache-redis-samples/quickstart/nodejs/node_modules/@redis/client/dist/lib/RESP/decoder.js:74:38)
+    at RedisSocket.<anonymous> (/workspaces/azure-cache-redis-samples/quickstart/nodejs/node_modules/@redis/client/dist/lib/client/index.js:399:37)
+    at RedisSocket.emit (node:events:518:28)
+    at TLSSocket.<anonymous> (/workspaces/azure-cache-redis-samples/quickstart/nodejs/node_modules/@redis/client/dist/lib/client/socket.js:205:38)
+    at TLSSocket.emit (node:events:518:28)
+    at addChunk (node:internal/streams/readable:561:12)
+    at readableAddChunkPushByteMode (node:internal/streams/readable:512:3)
+    at Readable.push (node:internal/streams/readable:392:5)
+*/
